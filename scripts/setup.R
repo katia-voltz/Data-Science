de984c848bf3bea73ee6ee57d0f75786eb80a0bd
katia-voltz <93271091+katia-voltz@users.noreply.github.com>
2021-12-07 15:17:50 +0100

    Update setup.R

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,8 +1,8 @@
 
 # load the required packages
 packages <- c(
-  "here","plyr","dplyr","tidyverse","tibble","corrplot","car","ggplot2","FactoMineR","factoextra","openxlsx","knitr","kableExtra", "bookdown", "rmarkdown","leaflet","plotly","cowplot")
+  "here","plyr","dplyr","tidyverse","tibble","corrplot","car","ggplot2","FactoMineR","factoextra","openxlsx","knitr","kableExtra", "bookdown", "rmarkdown","leaflet","plotly","cowplot","lme4","car")
 
 purrr::walk(packages, library, character.only = TRUE)
 
-
+options(digits = 5)
