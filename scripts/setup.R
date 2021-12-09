e182a1f1c08e3b2803199b3764ef94ea9488f280
nbidet <32996297+nbidet@users.noreply.github.com>
2021-12-09 09:52:29 +0100

    Update setup.R

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,7 +1,7 @@
 
 # load the required packages
 packages <- c(
-  "here","plyr","dplyr","tidyverse","tibble","corrplot","car","ggplot2","FactoMineR","factoextra","openxlsx","knitr","kableExtra", "bookdown", "rmarkdown","leaflet","plotly","cowplot","lme4","maps","ggpubr","ggrepel","carData")
+  "here","plyr","dplyr","tidyverse","tibble","corrplot","car","ggplot2","FactoMineR","factoextra","openxlsx","knitr","kableExtra", "bookdown", "rmarkdown","leaflet","plotly","cowplot","lme4","maps","ggpubr","ggrepel","carData","summarytools")
 
 purrr::walk(packages, library, character.only = TRUE)
 
